82d027a33091a1bd9784714cc43970cfe1c13b10
OneEurosphere <63873427+OneEurosphere@users.noreply.github.com>
2023-10-21 12:45:56 -0500

    MAH CODE BOYOS

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,64 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class Main extends JFrame { 
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	public Main () {
+		super("HackTX 2023");
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    	setSize(screenSize.width, screenSize.height);
+
+		Window window = new Window();
+		((Component) window).setFocusable(true);
+		setBackground(Color.GRAY);
+		getContentPane().add(window);
+
+		// GridLayout layout = new GridLayout(1, 2);
+		// setLayout(layout);
+
+		// ST
+
+		JFileChooser fc = new JFileChooser();
+
+		int num = screenSize.width / 2;
+        JTextArea tarea = new JTextArea(num, screenSize.height);
+        JButton readButton = new JButton("OPEN FILE");
+		readButton.setBounds(1, 1, 50, 50);
+        readButton.addActionListener(ev -> {
+        int returnVal = fc.showOpenDialog(window);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(
+                new FileInputStream(file)));
+            tarea.read(input, "READING FILE :-)");
+			tarea.setLineWrap(true);
+			tarea.setWrapStyleWord(true);
+            } catch (Exception e) {
+            e.printStackTrace();
+            }
+        } else {
+            System.out.println("Operation is CANCELLED :(");
         }
-    }
+        });
+
+		// END
+		JScrollPane scrollPane = new JScrollPane(tarea);
+		getContentPane().add(scrollPane);
+    	getContentPane().add(readButton, BorderLayout.PAGE_END);
+    	pack();
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 }
