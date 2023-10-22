a7aacd93f29eed9309473e0d19c9546e13a62fd6
EthanMunoz03 <142924410+EthanMunoz03@users.noreply.github.com>
2023-10-21 20:20:15 -0500

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,56 +1,185 @@
 import java.io.*;
-import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import javax.swing.*;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.*;
+import java.awt.BorderLayout;  
+import java.awt.Color;  
+import java.awt.Dimension;  
+import javax.swing.JButton;  
+import javax.swing.JFrame;  
+import javax.swing.JLabel;  
+import javax.swing.JScrollPane;  
+import javax.swing.border.LineBorder;
+import javax.swing.filechooser.FileSystemView; 
+import javax.swing.filechooser.FileFilter;
 
 public class GUI {
 	
-    // Main driver method
-    public static void main(String[] args) {
+	private static JDialog d;
+	private static String fromLanguage;
+	private static String toLanguage;
+	private static File uploadedFile;
+	private static ArrayList<String> highlightedWords;
+	
+    public static void main(String[] args) throws IOException {
     	try { 
     	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     	} catch (Exception e) {
     	    e.printStackTrace();
     	}
-        JFrame frame = new JFrame(); // creating instance of JFrame
- 
-        JLabel l1 = new JLabel("Select Wanted Language:");
+    	
+    	fromLanguage = "English";
+    	toLanguage = "English";
+    	ArrayList<String> highlightedWords = new ArrayList<String>();
+    	
+    	// creating instance of JFrame
+        JFrame frame = new JFrame(); 
         
-        l1.setBounds(50, 30, 200, 15);
-        frame.add(l1);
-  
-        final JLabel label = new JLabel();          
-        label.setSize(400, 100);  
-        label.setBounds(50, 85, 300, 20);
-        JButton b = new JButton("Choose");  
-        b.setBounds(200, 50, 75, 20);  
-        String languages[] = {"Spanish","French","Chinese", "Japanese", "Greek", "Austrian",
-        		"Finnish", "Swahili", "Russian", "Portugese", "Romanian"};        
-        final JComboBox cb = new JComboBox(languages);    
-        cb.setBounds(50, 50, 90, 20);    
-        frame.add(cb); frame.add(label); frame.add(b); 
+        //popUpWelcome(frame);
         
-        JTextArea text = new JTextArea("Paste Text Here");
-        text.setBounds(50, 110, 575, 485);
+        getFromLanguage(frame);
+        getToLanguage(frame);
+        
+        // big text box
+        JTextArea text = new JTextArea();
+        text.setBounds(50, 110, 575, 475);
         text.setLineWrap(true);
         text.setWrapStyleWord(true);
+        JScrollPane scroll = new JScrollPane(text);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scroll.add(text);
         frame.add(text);
         
-        b.addActionListener(new ActionListener() {  
+        uploadFileSetUp(frame, text);
+        
+        // Clear button
+        JButton clearButton = new JButton("Clear");
+        clearButton.setBounds(545, 590, 80, 20); // x axis, y axis, width, height
+        clearButton.addActionListener(new ActionListener() {  
             public void actionPerformed(ActionEvent e) {       
-            	String data = "Language Selected: "   + cb.getItemAt(cb.getSelectedIndex());  
-            	label.setText(data); 
+            	text.setText(null);
             }  
         });
+        frame.add(clearButton);
         
-        JButton button = new JButton("Done"); // creating instance of JButton
-        button.setBounds(545, 600, 80, 20); // x axis, y axis, width, height
-        frame.add(button); // adding button in JFrame
- 
+        // user highlighting words
+        
+        
+        
         frame.setSize(1275, 675); // 1275 width and 675 height
         frame.setLayout(null); // using no layout managers
         frame.setVisible(true); // making the frame visible
     }
     
+    // pop-up window
+    public static void popUpWelcome(JFrame frame) {
+    	d = new JDialog(frame, "Welcome to Translator App (name in progress)", true);  
+        d.setLayout(new FlowLayout());  
+        JButton but = new JButton("OK");  
+        but.addActionListener(new ActionListener()  {  
+            public void actionPerformed(ActionEvent e) {  
+                d.setVisible(false);  
+            }  
+        });  
+        d.add( new JLabel ("Welcome to this program!"));  
+        d.add(but);   
+        d.setSize(500, 500);
+        d.setLocationRelativeTo(frame);
+        d.setVisible(true);
+    }
+    
+    public static void getFromLanguage(JFrame frame) {
+    	// initial prompt
+        JLabel prompt = new JLabel("Select Input Language:");
+        prompt.setBounds(50, 30, 200, 15);
+        frame.add(prompt);
+        
+        // The dropdown menu
+        JButton b = new JButton("Set");
+        b.setBounds(150, 50, 50, 20);  
+        String languages[] = {"English", "Spanish","French","Chinese", "Japanese", "Greek", 
+        		"Austrian", "Finnish", "Swahili", "Russian", "Portugese", "Romanian"};       
+        final JComboBox cb = new JComboBox(languages);    
+        cb.setBounds(50, 50, 90, 20);    
+        frame.add(cb); frame.add(b); //frame.add(label);
+        
+        b.addActionListener(new ActionListener() {  
+            public void actionPerformed(ActionEvent e) {       
+            	fromLanguage = "" + cb.getItemAt(cb.getSelectedIndex());
+            }  
+        });
+    }
+    
+    public static void getToLanguage(JFrame frame) {
+    	// initial prompt
+        JLabel prompt = new JLabel("Select Translated Language:");
+        prompt.setBounds(450, 30, 200, 15);
+        frame.add(prompt);
+        
+        // The dropdown menu
+        JButton b = new JButton("Set");  
+        b.setBounds(550, 50, 50, 20);  
+        String languages[] = {"English", "Spanish","French","Chinese", "Japanese", "Greek", 
+        		"Austrian", "Finnish", "Swahili", "Russian", "Portugese", "Romanian"};       
+        final JComboBox cb = new JComboBox(languages);    
+        cb.setBounds(450, 50, 90, 20);    
+        frame.add(cb); frame.add(b); //.add(label);
+        
+        b.addActionListener(new ActionListener() {  
+            public void actionPerformed(ActionEvent e) {       
+            	toLanguage = "" + cb.getItemAt(cb.getSelectedIndex());
+            }  
+        });
+    }
+    
+    public static void uploadFileSetUp(JFrame frame, JTextArea text) {
+        JLabel filePrompt = new JLabel("Type in selected language or upload a .txt file:");
+        filePrompt.setBounds(50, 88, 500, 15);
+        frame.add(filePrompt);
+        
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.addChoosableFileFilter(new FileFilter() {
+            public String getDescription() {
+                return "T Documents (*.txt)";
+            }
+         
+            public boolean accept(File f) {
+                if (f.isDirectory()) {
+                    return true;
+                } else {
+                    return f.getName().toLowerCase().endsWith(".txt");
+                }
+            }
+        });
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        JButton fileButton = new JButton("Upload");
+        fileButton.setBounds(280, 85, 80, 20); // x axis, y axis, width, height
+        frame.add(fileButton);
+        fileButton.addActionListener(new ActionListener() {  
+            public void actionPerformed(ActionEvent e) {       
+            	int result = fileChooser.showOpenDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                	uploadedFile = fileChooser.getSelectedFile();
+                	try (BufferedReader reader = new BufferedReader(new FileReader(new File("" + 
+                			uploadedFile.getAbsolutePath())))) {
+                        text.read(reader, "File");
+                    } catch (IOException exp) {
+                        exp.printStackTrace();
+                    }
+                }
+            }  
+        });
+    }
+    
+    public static void something() {
+    	
+    }
 }
+
+public class D
